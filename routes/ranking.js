7369f142dbf548c535e758b4491a3c5d6f1ab8b4
Collin Van der Vorst <collinvandervorst@DESKTOP-A9EF0OJ.guests.thomasmore.be>
2025-03-03 12:07:20 +0100

    Add ranking

--- routes/ranking.js
+++ routes/ranking.js
@@ -1,10 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  const data = [];
-  res.json(data);
+router.get('/', async(req, res, next) => {
+  const votes = await prisma.votes.groupBy({
+    by: ['song_id'],
+    _sum: {
+      points: true,
+    },
+    orderBy: {
+      _sum: {
+        points: 'desc', // Sort by total points in descending order
+      },
+    }
+  });
+
+  let songs = await prisma.songs.findMany({
+    include: {
+      artists: true
+    }
+  });
+
+  songs = songs.map((song) => {
+    const points = votes.find((vote) => vote.song_id == song.song_id);
+
+    if (points) {
+      song.points = points._sum.points;
+    } else {
+      song.points = null;
+    }
+
+    return song;
+  })
+
+  res.json(songs);
 });
 
 module.exports = router;
--- routes/votes.js
+++ routes/votes.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
 /* GET users listing. */
-router.post('/', (req, res, next) => {
-  res.send("Just created new record");
+router.post('/', async (req, res, next) => {
+
+  const newVote = await prisma.votes.create({
+    data: { 
+      points: req.body.points,
+      song_id: req.body.song_id,
+      voter_id: req.body.voter_id
+    }
+  });
+
+  res.json(newVote);
 })
 
 module.exports = router;
